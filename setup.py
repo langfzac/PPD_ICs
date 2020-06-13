feb27787988037c10bf8ece10edbd2721f381ee4
Zach Langford <langfzac@uw.edu>
2020-06-12 21:53:28 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ import glob
 
 setup(
     name='mmic',
+    version='0.1',
     url='https://github.com/langfzac/mmic',
     packages=find_packages(),
     package_data={'mmic': ['utils/*']},
+    license='LICENSE',
     install_requires=["numpy","pytipsy","astropy"],
     include_package_data=True
 )
